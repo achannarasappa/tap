bb80b00eb752efb50c208544b68b08e2cc34afd4
achannarasappa <git@ani.dev>
2021-02-10 21:15:36 -0500

    Brew formula update for ticker version 3.0.7

--- ticker.rb
+++ ticker.rb
@@ -5,25 +5,25 @@
 class Ticker < Formula
   desc "Terminal stock ticker with live updates and position tracking"
   homepage "https://github.com/achannarasappa/ticker"
-  version "3.0.6"
+  version "3.0.7"
   license "GPLv3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/achannarasappa/ticker/releases/download/v3.0.6/ticker-3.0.6-mac-amd64.tar.gz"
-    sha256 "3db378d6cecb34008b800ce320046708067ca100114c6bb2fc0fc71d472cac97"
+    url "https://github.com/achannarasappa/ticker/releases/download/3.0.7/ticker-3.0.7-mac-amd64.tar.gz"
+    sha256 "9688fe08402e60f4e5efa266af82e428d7528965257bc5a6870d084cfaffe8ea"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/achannarasappa/ticker/releases/download/v3.0.6/ticker-3.0.6-linux-amd64.tar.gz"
-    sha256 "e2ded4c70c0cdddc78f2dafd90877fc7b11029a5e74abe610c7b85c308a3d1c7"
+    url "https://github.com/achannarasappa/ticker/releases/download/3.0.7/ticker-3.0.7-linux-amd64.tar.gz"
+    sha256 "8f31f953731d4001f186e6e47e77e82e737f2c1be7315bc1b0f618bb2579bc32"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/achannarasappa/ticker/releases/download/v3.0.6/ticker-3.0.6-linux-armv6.tar.gz"
-    sha256 "06a9a7acf9f8d4394d016bb761944661d2aa754f06a6d24b3630f0b443be501f"
+    url "https://github.com/achannarasappa/ticker/releases/download/3.0.7/ticker-3.0.7-linux-armv6.tar.gz"
+    sha256 "431ed4b7736921169beacf5433174661361484dea9d387442ae831db0f8cea8a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/achannarasappa/ticker/releases/download/v3.0.6/ticker-3.0.6-linux-arm64.tar.gz"
-    sha256 "218a0a821bb7bbc64254e00d29fcd66009ff1a904c30f6d83ac5ad07d2f22c27"
+    url "https://github.com/achannarasappa/ticker/releases/download/3.0.7/ticker-3.0.7-linux-arm64.tar.gz"
+    sha256 "c5dd345953c6656521c22293ab7aea74bd1a2776595b1aaf14710abeff38ce59"
   end
 
   def install
