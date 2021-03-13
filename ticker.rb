16db86ee8dca0b924e88b6b84e6429cc458fea46
achannarasappa <git@ani.dev>
2021-03-13 13:14:31 -0500

    Brew formula update for ticker version v4.0.5

--- ticker.rb
+++ ticker.rb
@@ -5,25 +5,25 @@
 class Ticker < Formula
   desc "Terminal stock ticker with live updates and position tracking"
   homepage "https://github.com/achannarasappa/ticker"
-  version "4.0.4"
+  version "4.0.5"
   license "GPLv3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.4/ticker-4.0.4-mac-amd64.tar.gz"
-    sha256 "0bcfb2ac82badcb68634e347d467c7207243b2b58ec94881bd56cd2d15ad458d"
+    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.5/ticker-4.0.5-mac-amd64.tar.gz"
+    sha256 "09b7fc81be4cbfae9d9a06e005ea41fdfb8740124a3cc71ce87a0ce38422595c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.4/ticker-4.0.4-linux-amd64.tar.gz"
-    sha256 "378b6ba831a4aca33aca91154e895e221dfd761046684a88e632a113fcec4bb4"
+    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.5/ticker-4.0.5-linux-amd64.tar.gz"
+    sha256 "ad99ab925f981115be98150ed8314af19ea7e90b6a5f90f528d94c6f8af41ae4"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.4/ticker-4.0.4-linux-armv6.tar.gz"
-    sha256 "b39fd5806917903eba559f9319fe4abd5351bf8e5c8ed75564d5724d1082c199"
+    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.5/ticker-4.0.5-linux-armv6.tar.gz"
+    sha256 "12779459128206d01bd8bf90da6e334f7c21dc26f3350eacf167de4950da8bdc"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.4/ticker-4.0.4-linux-arm64.tar.gz"
-    sha256 "bea635a7e3149620a333076f55eb99c83763418b6629eb0b13ed13bb4de9760c"
+    url "https://github.com/achannarasappa/ticker/releases/download/v4.0.5/ticker-4.0.5-linux-arm64.tar.gz"
+    sha256 "e73ee2aa35290a256f16e9ceaac26aff37b481a3e52631d647148052cd42d2c6"
   end
 
   def install
